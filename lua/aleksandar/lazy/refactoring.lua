05e68fa8d22d06a1daa9015490359321455c3272
DujinA <dujinaleksandar@gmail.com>
2026-05-24 14:58:02 +0200

    update

--- lua/aleksandar/lazy/refactoring.lua
+++ lua/aleksandar/lazy/refactoring.lua
@@ -2,82 +2,51 @@ return {
 	"ThePrimeagen/refactoring.nvim",
 
 	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
+		"lewis6991/async.nvim",
 	},
+
+	lazy = false,
+
 	config = function()
-		require("refactoring").setup({
-			prompt_func_return_type = {
-				go = false,
-				java = false,
+		local refactoring = require("refactoring")
+		local debug = require("refactoring.debug")
 
-				cpp = false,
-				c = false,
-				h = false,
-				hpp = false,
-				cxx = false,
-			},
-			prompt_func_param_type = {
-				go = false,
-				java = false,
-
-				cpp = false,
-				c = false,
-				h = false,
-				hpp = false,
-				cxx = false,
-			},
-			printf_statements = {},
-			print_var_statements = {},
-			show_success_message = false, -- shows a message with information about the refactor on success
-			-- i.e. [Refactor] Inlined 3 variable occurrences
-		})
+		-- Refactoring actions
 		vim.keymap.set({ "n", "x" }, "<leader>re", function()
-			return require("refactoring").refactor("Extract Function")
-		end, { expr = true })
+			return refactoring.extract_func()
+		end, { expr = true, desc = "Extract Function" })
+
 		vim.keymap.set({ "n", "x" }, "<leader>rf", function()
-			return require("refactoring").refactor("Extract Function To File")
-		end, { expr = true })
+			return refactoring.extract_func_to_file()
+		end, { expr = true, desc = "Extract Function To File" })
+
 		vim.keymap.set({ "n", "x" }, "<leader>rv", function()
-			return require("refactoring").refactor("Extract Variable")
-		end, { expr = true })
-		vim.keymap.set({ "n", "x" }, "<leader>rI", function()
-			return require("refactoring").refactor("Inline Function")
-		end, { expr = true })
+			return refactoring.extract_var()
+		end, { expr = true, desc = "Extract Variable" })
+
 		vim.keymap.set({ "n", "x" }, "<leader>ri", function()
-			return require("refactoring").refactor("Inline Variable")
-		end, { expr = true })
+			return refactoring.inline_var()
+		end, { expr = true, desc = "Inline Variable" })
 
-		vim.keymap.set({ "n", "x" }, "<leader>rbb", function()
-			return require("refactoring").refactor("Extract Block")
-		end, { expr = true })
-		vim.keymap.set({ "n", "x" }, "<leader>rbf", function()
-			return require("refactoring").refactor("Extract Block To File")
-		end, { expr = true })
+		vim.keymap.set({ "n", "x" }, "<leader>rI", function()
+			return refactoring.inline_func()
+		end, { expr = true, desc = "Inline Function" })
 
-		-- load refactoring Telescope extension
-		require("telescope").load_extension("refactoring")
+		vim.keymap.set({ "n", "x" }, "<leader>rs", function()
+			refactoring.select_refactor()
+		end, { desc = "Select Refactor" })
 
-		vim.keymap.set({ "n", "x" }, "<leader>rr", function()
-			require("telescope").extensions.refactoring.refactors()
-		end)
-
-		-- You can also use below = true here to to change the position of the printf
-		-- statement (or set two remaps for either one). This remap must be made in normal mode.
+		-- Debug helpers
 		vim.keymap.set("n", "<leader>rp", function()
-			require("refactoring").debug.printf({ below = false })
-		end)
+			return debug.print_loc({ output_location = "below" })
+		end, { expr = true, desc = "Debug Print Location" })
 
-		-- Print var
-
-		vim.keymap.set({ "x", "n" }, "<leader>rv", function()
-			require("refactoring").debug.print_var()
-		end)
-		-- Supports both visual and normal mode
+		vim.keymap.set({ "n", "x" }, "<leader>rP", function()
+			return debug.print_var({ output_location = "below" })
+		end, { expr = true, desc = "Debug Print Variable" })
 
 		vim.keymap.set("n", "<leader>rc", function()
-			require("refactoring").debug.cleanup({})
-		end)
-		-- Supports only normal mode
+			return debug.cleanup({ restore_view = true })
+		end, { expr = true, desc = "Cleanup Debug Prints" })
 	end,
 }
